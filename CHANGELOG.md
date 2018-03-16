c4747e5e4d1a58fa6bebb22ef9b72cfb6bc7d855
Fred Horch <fred@sparkae.com>
2018-03-16 12:08:30 -0400

    0.0.4
    Menus and spacing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog for meteormogul.dev
 
+## [0.0.4] - 2018-03-16
+
+- Menus and spacing
+
 ## [0.0.3] - 2018-03-16
 
 - Imports and routes in separate files
--- client/toolbar.js
+++ client/toolbar.js
@@ -7,7 +7,23 @@ var mmToolbar =
 {
   name: 'mm-toolbar',
   template: '#mm-toolbar-template',
-  mixins: [drawerMixin]
+  mixins: [drawerMixin],
+  data: () => ({
+      documentation: [
+        { title: 'Orientation', href: '/happy' },
+        { title: 'Survival Guide', href: '/sad' },
+        { title: 'Toolkit', href: '/happy' },
+        { title: 'Formulary', href: '/' }
+      ],
+      demos: [
+        { title: 'Hello, world!', href: 'https://meteor-mogul-hello-world.herokuapp.com/'},
+        { title: 'Meteor Mogul Vue Intro', href: 'https://meteor-mogul-vue-intro.herokuapp.com/'}
+      ],
+      repos: [
+        { title: 'Hello, world!', href: 'https://github.com/meteor-mogul/mogul-helloworld'},
+        { title: 'Meteor Mogul Vue Intro', href: 'https://github.com/meteor-mogul/vue-intro'}
+      ]
+    })
 };
 
 export { mmToolbar };
